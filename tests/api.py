b53307467f0b6078e24cf3073e948b8d162e95e9
arkadijsyckov <Z4mmer@gmail.com>
2025-06-23 00:11:42 +0500

    fix

---
+++ tests/api.py
@@ -0,0 +1,24 @@
+import requests
+
+from data.utils.logging_helper import logging_helper
+
+url_ = 'https://reqres.in/api'
+headers = {
+    "x-api-key": "reqres-free-v1"}
+
+
+def base_api(method, endpoint, **kwargs):
+    url = f"{url_}{endpoint}"
+    response = requests.request(
+        method=method,
+        url=url,
+        headers=headers,
+        **kwargs
+    )
+    logging_helper(response)
+    return response
+
+
+def post_login(payload):
+    response = base_api(method="POST", endpoint="/login/", json=payload)
+    return response
